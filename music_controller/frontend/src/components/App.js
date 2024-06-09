bd31d35b13f13933d78f951c5b22725cab2ac731
Tin Pham <nhattin55@gmail.com>
2024-06-09 00:19:20 -0700

    update router

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from "react";
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
+import HomePage from "./HomePage";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Music Controller</h1>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
-
-export default App;
 
 const appDiv = document.getElementById("app");
 const root = createRoot(appDiv);
